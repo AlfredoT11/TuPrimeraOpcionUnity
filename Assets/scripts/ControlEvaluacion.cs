3529d43158b82331627d67bbe9fa8c71217e5c76
Alfredo Diaz <alfredo117tdg@hotmail.com>
2019-10-27 05:51:47 -0600

    Una evaluación multiparametral completada

--- Assets/scripts/ControlEvaluacion.cs
+++ Assets/scripts/ControlEvaluacion.cs
@@ -9,6 +9,17 @@ public class ControlEvaluacion : MonoBehaviour
     string personajeSeleccionado;
     string medioBusquedaTrabajo;
     string vacanteElegida;
+    //Responsabilidad
+    List<object> lista12;
+    List<object> lista14;
+
+    //Manejo
+    List<object> lista23;
+    List<object> lista25;
+
+    //Espíritu colaborativo
+
+    //Comunicación para el impacto
     List<object> lista42;
 
     public void seleccionPersonaje(string personajeSeleccionado){
@@ -35,9 +46,53 @@ public class ControlEvaluacion : MonoBehaviour
         Debug.Log("4.2 Puntuacion: "+lista42[0]+" Cantidad: "+lista42[1]);
     }
 
+    public void eleccionClienteMolesto(List<int> valoresPonderados){
+        int auxPuntuacion = (int)lista12[0] + valoresPonderados[0];
+        lista12[0] = auxPuntuacion;
+        lista12[1] = (int)lista12[1] + 1;
+
+        auxPuntuacion = (int)lista14[0] + valoresPonderados[0];
+        lista14[0] = auxPuntuacion;
+        lista14[1] = (int)lista14[1] + 1; 
+
+        auxPuntuacion = (int)lista23[0] + valoresPonderados[0];
+        lista23[0] = auxPuntuacion;
+        lista23[1] = (int)lista23[1] + 1; 
+
+        auxPuntuacion = (int)lista25[0] + valoresPonderados[0];
+        lista25[0] = auxPuntuacion;
+        lista25[1] = (int)lista25[1] + 1;        
+
+        auxPuntuacion = (int)lista42[0] + valoresPonderados[0];
+        lista42[0] = auxPuntuacion;
+        lista42[1] = (int)lista42[1] + 1;       
+
+        Debug.Log("1.2 Puntuacion: "+lista12[0]+" Cantidad: "+lista12[1]);
+        Debug.Log("1.3 Puntuacion: "+lista14[0]+" Cantidad: "+lista14[1]);
+        Debug.Log("2.3 Puntuacion: "+lista23[0]+" Cantidad: "+lista23[1]);
+        Debug.Log("2.5 Puntuacion: "+lista25[0]+" Cantidad: "+lista25[1]);
+        Debug.Log("4.2 Puntuacion: "+lista42[0]+" Cantidad: "+lista42[1]);
+    }
+
     void Start()
     {
-    
+
+        lista12 = new List<object>();
+        lista12.Add(0); //Valor total.
+        lista12.Add(0); //Cantidad.        
+
+        lista14 = new List<object>();
+        lista14.Add(0); //Valor total.
+        lista14.Add(0); //Cantidad.
+
+        lista23 = new List<object>();
+        lista23.Add(0); //Valor total.
+        lista23.Add(0); //Cantidad.
+
+        lista25 = new List<object>();
+        lista25.Add(0); //Valor total.
+        lista25.Add(0); //Cantidad.
+
         lista42 = new List<object>();
         lista42.Add(0); //Valor total.
         lista42.Add(0); //Cantidad.
---
+++ Assets/scripts/EvaluacionClienteMolesto.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EvaluacionClienteMolesto : MonoBehaviour
+{
+    
+    public void evaluacionClienteMolesto(){
+        List<int> valoresPonderados = new List<int>(); //1.2, 1.4, 2.3, 2.5, 4.2
+        valoresPonderados.Add(0);
+        valoresPonderados.Add(0);
+        valoresPonderados.Add(0);
+        valoresPonderados.Add(0);
+        valoresPonderados.Add(0);
+
+        string mensajeClienteMolesto = this.gameObject.transform.GetChild(0).GetComponent<Text>().text[0].ToString();
+        Debug.Log("Cliente molesto: "+mensajeClienteMolesto);
+
+        switch(mensajeClienteMolesto){
+            case "1":
+                valoresPonderados[0] = 0;
+                valoresPonderados[1] = 0;
+                valoresPonderados[2] = 0;
+                valoresPonderados[3] = 0;
+                valoresPonderados[4] = 0;
+            break;
+            case "2":
+                valoresPonderados[0] = 5;
+                valoresPonderados[1] = 4;
+                valoresPonderados[2] = 5;
+                valoresPonderados[3] = 5;
+                valoresPonderados[4] = 5;
+            break;
+            case "3":
+                valoresPonderados[0] = 5;
+                valoresPonderados[1] = 5;
+                valoresPonderados[2] = 4;
+                valoresPonderados[3] = 3;
+                valoresPonderados[4] = 5;
+            break;
+            case "4":
+                valoresPonderados[0] = 0;
+                valoresPonderados[1] = 0;
+                valoresPonderados[2] = 0;
+                valoresPonderados[3] = 0;
+                valoresPonderados[4] = 0;
+            break;                                        
+        }        
+
+        GameObject.Find("ControladorPrincipal").SendMessage("eleccionClienteMolesto", valoresPonderados);
+
+    }
+
+
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(() => evaluacionClienteMolesto());
+    }
+
+}
